3d90a913810e0ca7876050e5cc679ef0a7048655
Ktaka95 <ktakada950711@gmail.com>
2022-02-12 21:20:55 +0900

    Some_Sums_before_turn_in

--- ABS/B_Some_Sums.py
+++ ABS/B_Some_Sums.py
@@ -6,12 +6,13 @@ for _ in range(4):
 	Ncpy = Ncpy / 10
 	if Ncpy >= 1:
 		div += 1
-factor = 0
 sum = 0
 for i in range(1, N + 1):
+	factor = 0
+	icpy = i
 	for j in range(div, -1, -1):
-		factor += i // (10 ** div)
-		i %= (10 ** div)
+		factor += icpy // (10 ** j)
+		icpy %= (10 ** j)
 	if A <= factor <= B:
-		sum += factor
+		sum += i
 print(sum)
