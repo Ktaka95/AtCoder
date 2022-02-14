fbcd5df2c861233efa9a7914f45f504fe3a54e05
Ktaka95 <ktakada950711@gmail.com>
2022-02-14 23:37:02 +0900

    Day33

--- ABS/C_Otoshidama.py
+++ ABS/C_Otoshidama.py
@@ -1,4 +1,15 @@
 N, Y = map(int, input().split())
+flag = False
 
-if Y < N * 1000 or N * 10000 < Y:
-	print("-1 -1 -1")
+for x in range(N + 1):
+	for y in range(N + 1 - x):
+		z = N - x - y
+		if 10000 * x + 5000 * y + 1000 * z == Y:
+			flag = True
+			break
+	if flag:
+		break
+if flag:
+	print(x, y, z)
+else:
+	print(-1, -1, -1)
