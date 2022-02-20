25d4952b82911333ff9c658c54140351eb4b19c5
Ktaka95 <ktakada950711@gmail.com>
2022-02-21 01:18:33 +0900

    https://youtu.be/dqxZYMgoEvo

---
+++ ABC/240/Example/C.py
@@ -0,0 +1,23 @@
+N, X = map(int, input().split())
+A = list()
+B = list()
+for _ in range(N):
+	a, b = map(int, input().split())
+	A.append(a)
+	B.append(b)
+
+dp = [[False] * (X + 1) for _ in range(N + 1)]
+dp[0][0] = True
+
+for i in range(N):
+	a = A[i]
+	b = B[i]
+	for j in range(X + 1):
+		if j + a <= X:
+			dp[i + 1][j + a] |= dp[i][j]
+		if j + b <= X:
+			dp[i + 1][j + b] |= dp[i][j]
+if dp[N][X]:
+	print("Yes")
+else:
+	print("No")
