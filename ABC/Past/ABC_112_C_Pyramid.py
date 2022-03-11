775be9a595b2ae20c3ad0b804a0dd6761b65c42a
Ktaka95 <ktakada950711@gmail.com>
2022-03-11 23:53:53 +0900

    Day44

--- ABC/Past/ABC_112_C_Pyramid.py
+++ ABC/Past/ABC_112_C_Pyramid.py
@@ -1,12 +1,18 @@
 N = int(input())
 x_y_h = [list(map(int, input().split())) for _ in range(N)]
 
-for X in range(101):
-	for Y in range(101):
-		for i in range(N):
-			H = x_y_h[i][2] + abs(x_y_h[i][0] - X) + abs(x_y_h[i][1] - Y)
-			for x, y, h in x_y_h:
-				calc = max(H - abs(x - X) - abs(y - Y), 0)
-				if calc != H:
-					break
-			print(X, Y, H)
+for x, y, h in x_y_h:
+	if h != 0:
+		tmp_x, tmp_y, tmp_h = x, y, h
+		break
+for cx in range(101):
+	for cy in range(101):
+		flag = True
+		H = abs(tmp_x - cx) + abs(tmp_y - cy) + tmp_h
+		for x, y, h in x_y_h:
+			calc_H = max(H - abs(x - cx) - abs(y - cy), 0)
+			if calc_H != h:
+				flag = False
+				break
+		if flag:
+			print(cx, cy, H)
