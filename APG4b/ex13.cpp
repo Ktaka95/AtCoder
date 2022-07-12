7bfa75747ee18d715fce851f7865bcf6d614f32e
Keisuke Takada <ktakada950711@gmail.com>
2022-07-12 18:15:35 +0900

    feat: ex13

---
+++ APG4b/ex13.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	int sum = 0;
+	vector<int> scores(N);
+	for (int i = 0; i < N; i++) {
+		cin >> scores.at(i);
+		sum += scores.at(i);
+	}
+	int ave = sum / N;
+	for (int i = 0; i < N; i++) {
+		cout << abs(scores.at(i) - ave) << endl;
+	}
+}
+
